b96fee5b0b02b2da1f84e5c573ff9e1501b6b31d
Isabelle <108989547+isabelledey@users.noreply.github.com>
2023-02-06 21:48:27 +0200

    created the calculation of individual operations

--- script.js
+++ script.js
@@ -1,51 +1,24 @@
-let result = 0;
-const message = "";
-const greeting = "Welcome to JS Calculator";
+const displayInput = document.querySelector("input[name='display']");
+const buttons = document.querySelectorAll("input[type='button']");
+const deleteBtn = document.querySelector(".delete");
+const clearBtn = document.querySelector(".clear");
 
-//Operator Functions
-function add(num_One, num_Two) {
-  result = parseInt(num_One) + parseInt(num_Two);
-  console.log(result);
-  return result;
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.value === "AC") {
+      displayInput.value = "";
+    } else if (e.target.value === "DE") {
+      displayInput.value = displayInput.value.slice(0, -1);
+    } else if (e.target.value === "=") {
+      displayInput.value = eval(displayInput.value);
+    } else {
+      displayInput.value += e.target.value;
+    }
+    console.log(displayInput.value);
+    console.log(typeof displayInput.value);
+  });
+});
 
-function subtract(num_One, num_Two) {
-  result = parseInt(num_One) - parseInt(num_Two);
-  console.log(result);
-  return result;
-}
+console.log(displayInput.value);
 
-function multiply(num_One, num_Two) {
-  result = parseInt(num_One) * parseInt(num_Two);
-  console.log(result);
-  return result;
-}
 
-function divide(num_One, num_Two) {
-  result = parseInt(num_One) / parseInt(num_Two);
-  console.log(result);
-  return result;
-}
-
-// Main Calculation Function;
-function operate(operation, number_One, number_Two) {
-  operation = prompt(`Enter the desired operation + , - , * , / `);
-  number_One = prompt("Enter the first number");
-  number_Two = prompt("Enter the second number");
-
-  if (operation === "+") {
-    return add(number_One, number_Two);
-  } else if (operation === "-") {
-    return subtract(number_One, number_Two);
-  } else if (operation === "*") {
-    return multiply(number_One, number_Two);
-  } else if (operation === "/") {
-    return divide(number_One, number_Two);
-  } else {
-    message = "Wrong Input";
-    return console.log(message);
-  }
-}
-
-console.log(greeting);
-operate();
